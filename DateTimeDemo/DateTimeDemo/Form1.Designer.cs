25908eafdf78cd1fc5857c054d079fa1978719e5
afhorton <106110920+afhorton@users.noreply.github.com>
2023-06-12 23:23:21 -0600

    Uploaded complete app.

---
+++ DateTimeDemo/DateTimeDemo/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DateTimeDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblToday = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpParty = new System.Windows.Forms.DateTimePicker();
+            this.lblMessage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblToday
+            // 
+            this.lblToday.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblToday.Location = new System.Drawing.Point(567, 21);
+            this.lblToday.Name = "lblToday";
+            this.lblToday.Size = new System.Drawing.Size(193, 37);
+            this.lblToday.TabIndex = 0;
+            this.lblToday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTime
+            // 
+            this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTime.Location = new System.Drawing.Point(617, 78);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(143, 37);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(445, 146);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(315, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(35, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Let\'s Party!!!";
+            // 
+            // dtpParty
+            // 
+            this.dtpParty.Location = new System.Drawing.Point(43, 109);
+            this.dtpParty.Name = "dtpParty";
+            this.dtpParty.Size = new System.Drawing.Size(275, 26);
+            this.dtpParty.TabIndex = 4;
+            this.dtpParty.ValueChanged += new System.EventHandler(this.dtpParty_ValueChanged);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.Color.DarkBlue;
+            this.lblMessage.Location = new System.Drawing.Point(46, 183);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 25);
+            this.lblMessage.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 399);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.dtpParty);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblToday);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblToday;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpParty;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ DateTimeDemo/DateTimeDemo/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimeDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // display current date and time
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayDateTimeNow();
+        }
+
+        // executes every second
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            DisplayDateTimeNow();
+        }
+
+        // display current date and time 
+        private void DisplayDateTimeNow ()
+        {
+            //lblToday.Text = DateTime.Now.ToLongDateString();
+            lblToday.Text = DateTime.Now.ToString("D");
+            //lblTime.Text = DateTime.Now.ToLongTimeString();
+            lblTime.Text = DateTime.Now.ToString("T");
+        }
+
+        // user selected date for the party
+        private void dtpParty_ValueChanged(object sender, EventArgs e)
+        {
+            DateTime partyDay = dtpParty.Value.Date; // ignore time 
+            // calculate how many sleeps
+            // difference of dates is measured in timespan - an internal way of measuring passage of time
+            TimeSpan difference = partyDay.Subtract(DateTime.Today);
+            int days = difference.Days; // measured in Days
+
+            lblMessage.Text = "The party is on " + partyDay.ToLongDateString();
+            if (days < 0) // in the past
+                lblMessage.Text += "\nSorry you missed it :-(";
+            else if (days == 0)
+                lblMessage.Text += "\nYay, It's today!!!";
+            else if (days == 1)
+                lblMessage.Text += "\nIt's tomorrow, one more sleep";
+            else
+                lblMessage.Text += "\n" + days.ToString() + " more sleeps";
+        }
+    }
+}
